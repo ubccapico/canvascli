29fdb8c748301ffd45765b08b79eb5990d6c1224
Joel Ostblom <joel.ostblom@gmail.com>
2021-05-30 20:20:06 -0700

    Add package versions for all dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='canvascli',
     version=__version__,
 
-    description=' A CLI to reformat and review Canvas grades',
+    description='A CLI to reformat and review Canvas grades',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 
@@ -19,12 +19,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'altair',
-        'canvasapi',
+        'altair>=4.1.0',
+        'canvasapi>=2.1.0',
         'click>=8.0.0',
-        'pandas',
-        'tabulate',
-        'dataclassy'
+        'pandas>=1.1.0',
+        'tabulate>=0.8.3',
+        'dataclassy>=0.10'
     ],
     entry_points={
         'console_scripts': [
